f95cdf3d7ee1bb7b5ee9206c48ec5bf267d9459b
hekunlin <rexlin600@gmail.com>
2020-01-03 01:14:32 +0800

    feat(github): add github api module

---
+++ spring-boot-github/src/main/java/xyz/rexlin600/GithubApplication.java
@@ -0,0 +1,17 @@
+package xyz.rexlin600;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author: rexlin600
+ * @date: 2020-01-03 01:13:47
+ */
+@SpringBootApplication
+public class GithubApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GithubApplication.class, args);
+    }
+
+}
