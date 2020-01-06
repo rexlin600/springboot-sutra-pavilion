5effe86f6c83bdfd32e75c486688e275a87bc5bc
hekunlin <rexlin600@gmail.com>
2020-01-06 17:39:27 +0800

    feat(swagger): 新增swagger服务

---
+++ spring-boot-swagger2/src/main/java/xyz/rexlin600/model/Message.java
@@ -0,0 +1,35 @@
+package xyz.rexlin600.model;
+
+import io.swagger.annotations.ApiModelProperty;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+import java.util.Date;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Message implements Serializable {
+
+    private Long id;
+
+    @ApiModelProperty(value = "消息体")
+    private String text;
+
+    @ApiModelProperty(value = "消息总结")
+    private String summary;
+
+    private Date createDate;
+
+    @Override
+    public String toString() {
+        return "Message{" +
+                "id=" + id +
+                ", text='" + text + '\'' +
+                ", summary='" + summary + '\'' +
+                ", createDate=" + createDate +
+                '}';
+    }
+}
---
+++ spring-boot-swagger2/src/main/java/xyz/rexlin600/model/User.java
@@ -0,0 +1,18 @@
+package xyz.rexlin600.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+
+    private Long id;
+
+    private String name;
+
+    private int age;
+
+}
