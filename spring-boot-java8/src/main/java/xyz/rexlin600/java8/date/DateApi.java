d8a1c786b2f899d9a7363ddb624076d109a31e65
hekunlin <rexlin600@gmail.com>
2020-01-10 10:37:36 +0800

    doc(xml)：更新xml

--- spring-boot-java8/src/main/java/xyz/rexlin600/java8/date/DateApi.java
+++ spring-boot-java8/src/main/java/xyz/rexlin600/java8/date/DateApi.java
@@ -1,10 +1,18 @@
 package xyz.rexlin600.java8.date;
 
+import lombok.extern.slf4j.Slf4j;
+
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+
 /**
  * DateApi 类
  *
  * @author: hekunlin
  * @date: 2020/1/9
  */
+@Slf4j
 public class DateApi {
+
+
 }
