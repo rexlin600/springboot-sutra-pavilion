864223e53bf7d11a03a1ec684634debe48f2f113
hekunlin <rexlin600@gmail.com>
2020-01-14 09:36:16 +0800

    doc(sql)：update db/*.sql

--- db/shiro_test.sql
+++ db/shiro_test.sql
@@ -1,36 +1,68 @@
 /*
- Navicat Premium Data Transfer
-
- Source Server         : docker_mysql_5.7.27
- Source Server Type    : MySQL
- Source Server Version : 50726
- Source Host           : localhost:3306
- Source Schema         : shiro_test
-
- Target Server Type    : MySQL
- Target Server Version : 50726
- File Encoding         : 65001
-
- Date: 13/01/2020 22:35:22
+SQLyog Ultimate v12.5.1 (64 bit)
+MySQL - 5.7.29 : Database - shiro_test
+*********************************************************************
 */
 
-SET NAMES utf8mb4;
-SET FOREIGN_KEY_CHECKS = 0;
+/*!40101 SET NAMES utf8 */;
 
--- ----------------------------
--- Table structure for role
--- ----------------------------
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`shiro_test` /*!40100 DEFAULT CHARACTER SET utf8mb4 */;
+
+USE `shiro_test`;
+
+/*Table structure for table `resource` */
+
+DROP TABLE IF EXISTS `resource`;
+
+CREATE TABLE `resource` (
+  `id` bigint(20) NOT NULL COMMENT '主键ID',
+  `parent_id` bigint(20) DEFAULT NULL COMMENT '父ID',
+  `resource_name` varchar(50) DEFAULT NULL COMMENT '资源名称',
+  `type` int(11) DEFAULT NULL COMMENT '资源类型 1-菜单 2-页面 3-按钮',
+  `type_name` varchar(50) DEFAULT NULL COMMENT '资源类型名称',
+  `sort` int(11) DEFAULT NULL COMMENT '序号',
+  `icon` varchar(500) DEFAULT NULL COMMENT '资源icon',
+  `http_method` varchar(10) DEFAULT NULL COMMENT 'HTTP请求方法',
+  `http_url` varchar(100) DEFAULT NULL COMMENT 'HTTP请求路径',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='资源表';
+
+/*Data for the table `resource` */
+
+/*Table structure for table `role` */
+
 DROP TABLE IF EXISTS `role`;
+
 CREATE TABLE `role` (
   `id` bigint(20) NOT NULL COMMENT '主键',
   `role_name` varchar(50) DEFAULT NULL COMMENT '角色名称',
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='角色表';
 
--- ----------------------------
--- Table structure for user
--- ----------------------------
+/*Data for the table `role` */
+
+/*Table structure for table `role_resource` */
+
+DROP TABLE IF EXISTS `role_resource`;
+
+CREATE TABLE `role_resource` (
+  `role_id` bigint(20) NOT NULL COMMENT '角色ID',
+  `resource_id` bigint(20) NOT NULL COMMENT '资源ID',
+  PRIMARY KEY (`role_id`,`resource_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='角色资源表';
+
+/*Data for the table `role_resource` */
+
+/*Table structure for table `user` */
+
 DROP TABLE IF EXISTS `user`;
+
 CREATE TABLE `user` (
   `id` bigint(20) NOT NULL COMMENT '主键ID',
   `username` varchar(50) DEFAULT NULL COMMENT '用户名',
@@ -39,14 +71,21 @@ CREATE TABLE `user` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='用户表';
 
--- ----------------------------
--- Table structure for user_role
--- ----------------------------
+/*Data for the table `user` */
+
+/*Table structure for table `user_role` */
+
 DROP TABLE IF EXISTS `user_role`;
+
 CREATE TABLE `user_role` (
   `user_id` bigint(20) NOT NULL COMMENT '用户ID',
   `role_id` bigint(20) NOT NULL COMMENT '角色ID',
   PRIMARY KEY (`user_id`,`role_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='用户-角色关联表';
 
-SET FOREIGN_KEY_CHECKS = 1;
+/*Data for the table `user_role` */
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
--- db/tb_flyway.sql
+++ db/tb_flyway.sql
@@ -1,26 +1,25 @@
 /*
- Navicat Premium Data Transfer
-
- Source Server         : docker_mysql_5.7.27
- Source Server Type    : MySQL
- Source Server Version : 50726
- Source Host           : localhost:3306
- Source Schema         : spring-boot-mybatisplus
-
- Target Server Type    : MySQL
- Target Server Version : 50726
- File Encoding         : 65001
-
- Date: 11/01/2020 15:27:01
+SQLyog Ultimate v12.5.1 (64 bit)
+MySQL - 5.7.29 : Database - tb_flyway
+*********************************************************************
 */
 
-SET NAMES utf8mb4;
-SET FOREIGN_KEY_CHECKS = 0;
+/*!40101 SET NAMES utf8 */;
 
--- ----------------------------
--- Table structure for tb_flyway
--- ----------------------------
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`tb_flyway` /*!40100 DEFAULT CHARACTER SET utf8mb4 */;
+
+USE `tb_flyway`;
+
+/*Table structure for table `tb_flyway` */
+
 DROP TABLE IF EXISTS `tb_flyway`;
+
 CREATE TABLE `tb_flyway` (
   `id` bigint(20) DEFAULT NULL,
   `name` varchar(255) DEFAULT NULL,
@@ -28,4 +27,9 @@ CREATE TABLE `tb_flyway` (
   `create_time` datetime DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
-SET FOREIGN_KEY_CHECKS = 1;
+/*Data for the table `tb_flyway` */
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
