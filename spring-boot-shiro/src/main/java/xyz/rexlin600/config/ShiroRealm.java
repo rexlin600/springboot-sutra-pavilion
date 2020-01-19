ca1ca23dc2c222daf3c2522b2f2bc87a0a5038db
hekunlin <rexlin600@gmail.com>
2020-01-19 21:58:55 +0800

    feat(shiro): add shiro code

---
+++ spring-boot-shiro/src/main/java/xyz/rexlin600/config/ShiroRealm.java
@@ -0,0 +1,51 @@
+package xyz.rexlin600.config;
+
+import org.apache.shiro.authc.AuthenticationException;
+import org.apache.shiro.authc.AuthenticationInfo;
+import org.apache.shiro.authc.AuthenticationToken;
+import org.apache.shiro.authc.credential.CredentialsMatcher;
+import org.apache.shiro.authz.AuthorizationInfo;
+import org.apache.shiro.realm.AuthorizingRealm;
+import org.apache.shiro.subject.PrincipalCollection;
+
+/**
+ * @author: rexlin600
+ * @date: 2020-01-19
+ */
+public class ShiroRealm extends AuthorizingRealm {
+
+
+    /**
+     * 密码匹配器
+     *
+     * @param credentialsMatcher
+     */
+    @Override
+    public void setCredentialsMatcher(CredentialsMatcher credentialsMatcher) {
+
+    }
+
+    /**
+     * 认证
+     *
+     * @param principalCollection
+     * @return
+     */
+    @Override
+    protected AuthorizationInfo doGetAuthorizationInfo(PrincipalCollection principalCollection) {
+        return null;
+    }
+
+    /**
+     * 授权
+     *
+     * @param authenticationToken
+     * @return
+     * @throws AuthenticationException
+     */
+    @Override
+    protected AuthenticationInfo doGetAuthenticationInfo(AuthenticationToken authenticationToken) throws AuthenticationException {
+        return null;
+    }
+
+}
