ea12dfac65610c225cf343c0beec55a1ac812e12
hekunlin <rexlin600@gmail.com>
2020-02-14 22:35:07 +0800

    feat(aop): add aop beta

---
+++ spring-boot-aop/src/main/java/xyz/rexlin600/AopApplication.java
@@ -0,0 +1,18 @@
+package xyz.rexlin600;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ *
+ * @author: rexlin600
+ * @date: 2020-02-12
+ */
+@SpringBootApplication
+public class AopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AopApplication.class, args);
+    }
+
+}
