376667732cf9d7ca0137886ee69dbead0646ec28
hekunlin <rexlin600@gmail.com>
2020-02-28 17:25:44 +0800

    feat(qrcode): 新增二维码功能

---
+++ spring-boot-qrcode/src/main/java/xyz/rexlin600/qrcode/QrCodeApplication.java
@@ -0,0 +1,19 @@
+package xyz.rexlin600.qrcode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 二维码启动类
+ *
+ * @author: rexlin600
+ * @date: 2020-02-28
+ */
+@SpringBootApplication
+public class QrCodeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QrCodeApplication.class, args);
+    }
+
+}
