762c873ac8778db14c7970b48be10d254e1d61ad
hekunlin <hekunlin@hikcreate.com>
2020-06-28 17:36:56 +0800

    feat(kafka): 增加kafka module

---
+++ spring-boot-kafka/src/main/java/xyz/rexlin600/kafka/KafkaApplication.java
@@ -0,0 +1,19 @@
+package xyz.rexlin600.kafka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Kafka 启动类
+ *
+ * @author: hekunlin
+ * @date: 2020/6/28
+ */
+@SpringBootApplication
+public class KafkaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KafkaApplication.class, args);
+    }
+
+}
